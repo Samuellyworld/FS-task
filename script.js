8421830a0adfc415536ff07edc35045004b1a4ab
samuel <samuellyworld@gmail.com>
2021-04-18 01:05:36 -0700

    adding starting file

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
+const path = require('path');
+
+let filePath = path.join(__dirname, 'result', '/' ? 'posts.json' : '/' )
 
 const getPosts = fetch => {
 	fetch('http://jsonplaceholder.typicode.com/posts')
 	.then(response => response.json())
 	.then(data => {
 		if(data) {
-			fs.writeFile('result/posts.json',JSON.stringify(data), (err) => {
+			fs.writeFile(filePath, JSON.stringify(data), (err) => {
 				if(err) throw err
 			})
 		}	
